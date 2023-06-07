edb90a548af136657254d7879645a5e8c3984d52
Al Miha <contact.almiha@gmail.com>
2023-06-07 21:03:37 +0200

    replace scripts for libraries to Project 4

--- script.js
+++ script.js
@@ -12,14 +12,27 @@ $(document).ready(function () {
     // }
   });
 
-  // $(window).scroll(function () {
-  //   let position = $(this).scrollTop();
-  //   if (position >= 200) {
-  //     $(".nav-menu").addClass("custom-navbar");
-  //   } else {
-  //     $(".nav-menu").removeClass("custom-navbar");
-  //   }
-  // });
+  lightbox.option({
+    wrapAround: true,
+  });
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+
+    if (position >= 350) {
+      $(".gallery").addClass("change");
+    } else {
+      $(".gallery").removeClass("change");
+    }
+  });
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 200) {
+      $(".nav-menu").addClass("custom-navbar");
+    } else {
+      $(".nav-menu").removeClass("custom-navbar");
+    }
+  });
 
   // $(window).scroll(function () {
   //   let position = $(this).scrollTop();
